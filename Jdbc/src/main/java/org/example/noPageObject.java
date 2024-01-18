5eea50a5935aa944dc1d8c9525bbd24b4f78adf8
Slaveik-1 <125739630+Slaveik-1@users.noreply.github.com>
2024-01-18 23:56:17 +0400

    Add files via upload

--- Jdbc/src/main/java/org/example/noPageObject.java
+++ Jdbc/src/main/java/org/example/noPageObject.java
@@ -8,24 +8,35 @@ public class noPageObject {
             "jdbc:h2:tcp://localhost:9092/mem:testdb",
             "user",
             "pass");
-        String insertOne = "   INSERT INTO FOOD(food_id, food_name, food_type, food_exotic) VALUES" +
-                "(31,'Мандарин','FRUIT',1), " +
-                "(32,'Цукини','VEGETABLE',0)";
-            String query = "SELECT food_id, food_name, food_type, food_exotic FROM FOOD";
-            Statement statement = connection.createStatement();
+    String odinn = "SELECT food_id, food_name, food_type, food_exotic FROM FOOD";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(odinn);
+        while (resultSet.next()){
+            int id = resultSet.getInt("food_id");
+            String name = resultSet.getString("food_name");
+            String type = resultSet.getString("food_type");
+            int ex = resultSet.getInt("food_exotic");
+            System.out.printf("%d, %s, %s, %d%n", id, name, type, ex);
+            //Получаем изначальную таблицу
+        }
+        System.out.println("Очень важный пробел что бы не поплыли глаза на таблицах");
+        String insertOne = "   INSERT INTO FOOD(food_name, food_type, food_exotic) VALUES" +
+                "('Мандарин','FRUIT',1)," +
+                "('Цукини','VEGETABLE',0);";
+            String query = "SELECT * FROM FOOD order by food_id desc limit 2";
         statement.executeUpdate(insertOne);
-            ResultSet resultSet = statement.executeQuery(query);
+            resultSet = statement.executeQuery(query);
             while (resultSet.next()){
                 int id = resultSet.getInt("food_id");
                 String name = resultSet.getString("food_name");
                 String type = resultSet.getString("food_type");
                 int ex = resultSet.getInt("food_exotic");
                 System.out.printf("%d, %s, %s, %d%n", id, name, type, ex);
+                //Добавилось ?
             }
-        System.out.println("а удалиться ли оно вот так ? ");
+        System.out.println("Очень важный пробел что бы не поплыли глаза на таблицах");
 
-            statement.executeUpdate("DELETE FROM FOOD WHERE FOOD_ID=31");
-        statement.executeUpdate("DELETE FROM FOOD WHERE FOOD_ID=32");
+            statement.executeUpdate("DELETE FROM FOOD WHERE FOOD_ID in (SELECT FOOD_ID FROM FOOD ORDER BY FOOD_ID DESC LIMIT 2)");
             query = "SELECT * FROM FOOD";
 
         resultSet = statement.executeQuery(query);
@@ -35,6 +46,8 @@ public class noPageObject {
             String type = resultSet.getString("food_type");
             int ex = resultSet.getInt("food_exotic");
             System.out.printf("%d, %s, %s, %d%n", id, name, type, ex);
+            //Удалилось ?
+
         }
             connection.close();
 
